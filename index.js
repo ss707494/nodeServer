1748612ad3156809639a21aec51d0c0da43d3a77
sshen <sshen@fiberhome.com>
2017-12-08 15:51:50 +0800

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const proxy = require('express-http-proxy');
 const winston = require('winston'),
       expressWinston = require('express-winston');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8040;
 const app = express();
 
 const appPath = 'webapp';
